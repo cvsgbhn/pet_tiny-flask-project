696d6d582f23c1bfb8f136eae41e4b6069c58c10
Анна Дубовик <dubovik-a@pik-arenda.ru>
2020-01-10 18:50:02 +0300

    add actual words

---
+++ api.py
@@ -0,0 +1,42 @@
+import os
+
+import flask
+from flask import Flask, request, jsonify, render_template
+import random
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+def take_home_memes():
+    memes = os.listdir("./static/")
+    for i in memes:
+        i = str('/static/' + i + ' ')
+    return memes
+
+
+def shut_up_and_shuffle(array):
+    shuffled_array = []
+    i = 0
+    while i < len(array):
+        current_selection = random.choice(array)
+        if current_selection not in shuffled_array:
+            shuffled_array.append(current_selection)
+        i = i+1
+    return shuffled_array
+
+
+@app.route('/', methods=['GET'])
+def show_picture():
+    memes = take_home_memes()
+    mem = random.choice(memes)
+    mem = '/static/' + mem
+    return render_template('home.html', mem=mem)
+
+
+app.run()
