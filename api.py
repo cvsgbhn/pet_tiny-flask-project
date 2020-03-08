cb046dd5ee112031178804c3b6e5474ddce98dd5
cvsgbhn <kbh123456g@gmail.com>
2020-03-09 01:07:45 +0300

    TODO: write meaningful commit message

--- api.py
+++ api.py
@@ -7,16 +7,12 @@ import random
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET','POST'])
-def universe_button_tap():
-    return # name of universe folder
-
 @app.errorhandler(404)
 def page_not_found(arg):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
-def take_home_memes():
+def take_home_memes(universe):
     memes = os.listdir("./static/images/witcher")
     return memes
 
@@ -34,9 +30,19 @@ def shut_up_and_shuffle(array):
 
 @app.route('/')
 def show_picture():
-    memes = take_home_memes()
+    if request.method == 'POST':
+        if request.form['universe_button'] == 'witcher':
+            path = "./static/images/witcher"
+            memes = os.listdir(path)
+        if request.form['universe button'] == 'DC':
+            path = "./static/images/dc"
+            memes = os.listdir(path)
+    else:
+        #TODO: add collecting pics from all images/* folders
+        path = "./static/images/witcher"
+        memes = os.listdir("./static/images/witcher")
     mem = random.choice(memes)
-    mem = './static/images/witcher/' + mem
+    mem = path + '/' + mem
     universe_name = mem.split("/")[3]
     return render_template('home.html', mem=mem, universe_name=universe_name)
 
