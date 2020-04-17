9e99433df006b7e4bdb8ca50f69d624b42a03ca7
cvsgbhn <kbh123456g@gmail.com>
2020-04-17 22:42:31 +0300

    ajax for next picture

--- api.py
+++ api.py
@@ -11,12 +11,6 @@ app.config["DEBUG"] = True
 def page_not_found(arg):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-
-def take_home_memes(universe):
-    memes = os.listdir("./static/images/witcher")
-    return memes
-
-
 def shut_up_and_shuffle(array):
     shuffled_array = []
     i = 0
@@ -27,24 +21,37 @@ def shut_up_and_shuffle(array):
         i = i+1
     return shuffled_array
 
+@app.route('/getimage')
+def get_img():
+    universe = request.args['universe'];
+    print(universe)
+    memes = os.listdir("./static/images/{}".format(universe))
+    return random.choice(memes)
 
 @app.route('/')
-def show_picture():
-    if request.method == 'POST':
-        if request.form['universe_button'] == 'witcher':
-            path = "./static/images/witcher"
-            memes = os.listdir(path)
-        if request.form['universe button'] == 'DC':
-            path = "./static/images/dc"
-            memes = os.listdir(path)
-    else:
-        #TODO: add collecting pics from all images/* folders
-        path = "./static/images/witcher"
-        memes = os.listdir("./static/images/witcher")
+@app.route('/witcher')
+def get_witcher_picture():
+    #TODO: add collecting pics from all images/* folders
+    path = "./static/images/witcher"
+    memes = os.listdir("./static/images/witcher")
     mem = random.choice(memes)
     mem = path + '/' + mem
     universe_name = mem.split("/")[3]
-    return render_template('home.html', mem=mem, universe_name=universe_name)
+    return render_template('home.html', mem=mem, title=universe_name)
 
+@app.route('/dc')
+def get_dc_picture():
+    #TODO: add collecting pics from all images/* folders
+    path = "./static/images/dc"
+    memes = os.listdir("./static/images/dc")
+    mem = random.choice(memes)
+    mem = path + '/' + mem
+    universe_name = mem.split("/")[3]
+    return render_template('home.html', mem=mem, title=universe_name)
 
-app.run()
+@app.route('/to_universe')
+def get_universe_route():
+    return redirect(url_for('/dc'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
